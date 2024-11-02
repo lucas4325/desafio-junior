53a3ff04f857b9677d1c31c83c52aec238dba930
Lucas Emanoel <lucasemanoelfq2005@gmail.com>
2024-11-02 19:55:47 -0300

    commit inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+
+export default function Home() {
+  
+  
+  return (
+    <div className={styles.home}>
+      <div className={styles.titulo}>
+        
+      </div>
+
+      <div className={styles.containerTarefas}>
+        <div className={styles.tarefas}>
+
+        </div>
+      </div>
+    </div>
+  );
+}
