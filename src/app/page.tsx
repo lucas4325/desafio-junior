2f747762f4e5cabb82308d7b731bcda7938c1f91
Lucas Emanoel <lucasemanoelfq2005@gmail.com>
2024-11-04 07:29:01 -0300

    adiciona nova tarefa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
+"use client"
+
+import Linha from "@/components/linha";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 
-// const lista = [
+const listaM:any = [
 
-// ]
+]
 
 export default function Home() {
+  const [ lista, setLista ] = useState(listaM)
+
+  const addTarefa = ()=>{
+    setLista((values:any) =>{
+      return([
+        ...values,
+        {
+          titulo:'',
+          descricao: '',
+          prioridade: '',
+          concluida: false,
+        }
+      ])
+    })
+  }
   
-  
+  useEffect(()=>{
+    console.log(lista);
+  }, [lista])
   return (
     <div className={styles.home}>
       <div className={styles.containerTitulo}>
@@ -16,7 +37,24 @@ export default function Home() {
 
       <div className={styles.containerTarefas}>
         <div className={styles.tarefas}>
+          <button onClick={addTarefa} className={styles.adicionar} > Adicionar</button>
 
+          <div className={styles.headerTabela}>
+            <span>Titulo</span>
+            <span>Descrição</span>
+            <span>Prioridade</span>
+            <span>Concluido</span>
+          </div>
+
+          <div className={styles.conteudoTabela} >
+            {
+              lista.map((e:any, i:number)=>{
+                return(
+                  <Linha key={i} />
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     </div>
